7a55bfe86066f7044457bdc6a3f16ba274aa8d16
Chillhumanoid <jonathanthorne13@gmail.com>
2019-09-10 07:45:44 -0400

    Added Notification capability

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/MainActivity.java
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/MainActivity.java
@@ -1,8 +1,11 @@
 package com.theunquenchedservant.granthornersbiblereadingsystem;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +17,8 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -40,6 +45,55 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        createNotificationChannel();
+        notificationMaker();
+    }
+    private void notificationMaker(){
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM dd");
+        String str_today = formatter.format(today);
+        String content = "";
+        String list1 = getListContent("List 1", R.array.list_1);
+        String list2 = getListContent("List 2", R.array.list_2);
+        String list3 = getListContent("List 3", R.array.list_3);
+        String list4 = getListContent("List 4", R.array.list_4);
+        String list5 = getListContent("List 5", R.array.list_5);
+        String list6 = getListContent("List 6", R.array.list_6);
+        String list7 = getListContent("List 7", R.array.list_7);
+        String list8 = getListContent("List 8", R.array.list_8);
+        String list9 = getListContent("List 9", R.array.list_9);
+        String list10 = getListContent("List 10", R.array.list_10);
+        content = list1 + "\n" + list2 + "\n" + list3 + "\n" + list4 + "\n" + list5 + "\n" + list6 + "\n" + list7 + "\n" + list8 + "\n" + list9 + "\n" + list10;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "daily")
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle(str_today)
+                .setContentText(content)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(content))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(3, builder.build());
+    }
+    private String getListContent(String listString, int ArrayId){
+        Resources res = getResources();
+        String[] list = res.getStringArray(ArrayId);
+        int number = getListNumber(listString);
+        return list[number];
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("daily", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
     public void setChecked(View view){
         Switch psSwitch = (Switch)view.findViewById(R.id.psalms_switch);
