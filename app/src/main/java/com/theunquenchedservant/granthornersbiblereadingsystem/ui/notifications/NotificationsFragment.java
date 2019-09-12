a3cdf76d7ff4c9061046709f24c3234d072ce2e6
Chillhumanoid <jonathanthorne13@gmail.com>
2019-09-11 22:31:27 -0400

    Now creates working AlarmManager for notifications if switch is on

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/NotificationsFragment.java
@@ -2,6 +2,8 @@ package com.theunquenchedservant.granthornersbiblereadingsystem.ui.notifications
 
 import android.app.AlarmManager;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,49 +11,42 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity;
 import com.theunquenchedservant.granthornersbiblereadingsystem.R;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
-import static com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity.deliverNotification;
-
+//TODO Add time option
+//TODO Add save button (only update alarmmanager when saved)
 public class NotificationsFragment extends Fragment {
-    private AlarmManager alarmManager;
-    private NotificationsViewModel notificationsViewModel;
-    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID=0;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        NotificationsViewModel notificationsViewModel = ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         Switch notificationToggle = root.findViewById(R.id.notificationToggle);
+        if(MainActivity.prefReadInt(getActivity(), "notifications") == 1){
+            notificationToggle.setChecked(true);
+        }else{
+            notificationToggle.setChecked(false);
+        }
         notificationToggle.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                         String toastMessage;
-                        Calendar calendar = Calendar.getInstance();
-                        calendar.setTimeInMillis(System.currentTimeMillis());
-                        calendar.set(Calendar.HOUR_OF_DAY, 8);
-                        calendar.set(Calendar.MINUTE, 0);
-
+                        Intent notifyIntent = new Intent(getActivity(), AlarmReceiver.class);
+                        PendingIntent notifyPendingIntent = PendingIntent.getBroadcast(getActivity(), NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
                         if(isChecked){
-                            //Set the toast message for the "on" case.
-                            alarmManager.setInexactRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+                            MainActivity.prefEditInt(getActivity(), "notifications", 1);
+                            MainActivity.createAlarm(getActivity(), notifyPendingIntent);
                             toastMessage = "Notifications On";
                         } else {
-                            //Set the toast message for the "off" case.
-                            mNotificationManager.cancelAll();
+                            MainActivity.prefEditInt(getActivity(), "notifications", 0);
+                            MainActivity.cancelAlarm(getActivity(), notifyPendingIntent);
                             toastMessage = "Notifications Off";
                         }
 
