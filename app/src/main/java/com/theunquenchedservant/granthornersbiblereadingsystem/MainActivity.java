4efc7fa7bcc0509669e825b2af6f7838ca134b69
Chillhumanoid <jonathanthorne13@gmail.com>
2019-09-16 16:47:37 -0400

    removed default comments

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/MainActivity.java
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/MainActivity.java
@@ -38,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
