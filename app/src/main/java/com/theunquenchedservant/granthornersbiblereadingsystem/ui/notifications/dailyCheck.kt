4ad8dc00e579fbc7451be0319f479ec48b9eeb1f
Chillhumanoid <jonathanthorne13@gmail.com>
2019-09-22 22:20:33 -0400

    Daily check now accurately handles streaks and resets for partial lists

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/dailyCheck.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/dailyCheck.kt
@@ -4,14 +4,29 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.preference.Preference
+import androidx.preference.PreferenceManager
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity
+import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity.Companion.prefEditInt
+import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity.Companion.prefReadInt
 
 class dailyCheck : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        val check = MainActivity.prefReadString(context, "dateClicked")
-        val str_today = MainActivity.getYesterdayDate(false)
-        if (check != str_today) {
-            MainActivity.prefEditInt(context, "curStreak", 0)
+        when(prefReadInt(context, "dailyStreak")) {
+            1 -> prefEditInt(context, "dailyStreak", 0)
+            0 -> prefEditInt(context, "curStreak", 0)
         }
+        //RESETS ALL DONE LISTS AT MIDNIGHT EACH NIGHT
+        when(prefReadInt(context, "list1Done")){1->{ prefEditInt(context, "List 1", prefReadInt(context, "List 1") + 1); prefEditInt(context, "list1Done", 0)}}
+        when(prefReadInt(context, "list2Done")){1->{ prefEditInt(context, "List 2", prefReadInt(context, "List 2") + 1); prefEditInt(context, "list2Done", 0)}}
+        when(prefReadInt(context, "list3Done")){1->{ prefEditInt(context, "List 3", prefReadInt(context, "List 3") + 1); prefEditInt(context, "list3Done", 0)}}
+        when(prefReadInt(context, "list4Done")){1->{ prefEditInt(context, "List 4", prefReadInt(context, "List 4") + 1); prefEditInt(context, "list4Done", 0)}}
+        when(prefReadInt(context, "list5Done")){1->{ prefEditInt(context, "List 5", prefReadInt(context, "List 5") + 1); prefEditInt(context, "list5Done", 0)}}
+        when(prefReadInt(context, "list6Done")){1->{ prefEditInt(context, "List 6", prefReadInt(context, "List 6") + 1); prefEditInt(context, "list6Done", 0)}}
+        when(prefReadInt(context, "list7Done")){1->{ prefEditInt(context, "List 7", prefReadInt(context, "List 7") + 1); prefEditInt(context, "list7Done", 0)}}
+        when(prefReadInt(context, "list8Done")){1->{ prefEditInt(context, "List 8", prefReadInt(context, "List 8") + 1); prefEditInt(context, "list8Done", 0)}}
+        when(prefReadInt(context, "list9Done")){1->{ prefEditInt(context, "List 9", prefReadInt(context, "List 9") + 1); prefEditInt(context, "list9Done", 0)}}
+        when(prefReadInt(context, "list10Done")){1->{ prefEditInt(context, "List 10", prefReadInt(context, "List 10") + 1); prefEditInt(context, "list10Done", 0)}}
+        prefEditInt(context, "listsDone", 0)
     }
 }
--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/doneReceiver.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/doneReceiver.kt
@@ -8,23 +8,22 @@ import android.widget.Toast
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity.Companion.getCurrentDate
 import com.theunquenchedservant.granthornersbiblereadingsystem.R
-import com.theunquenchedservant.granthornersbiblereadingsystem.ui.home.HomeFragment
 
 class doneReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val check = MainActivity.prefReadString(context, "dateClicked")
         val today = getCurrentDate(false)
         if (check != today) {
-            MainActivity.markList(context, "List 1", R.array.list_1)
-            MainActivity.markList(context,"List 2", R.array.list_2)
-            MainActivity.markList(context,"List 3", R.array.list_3)
-            MainActivity.markList(context,"List 4", R.array.list_4)
-            MainActivity.markList(context,"List 5", R.array.list_5)
-            MainActivity.markList(context,"List 6", R.array.list_6)
-            MainActivity.markList(context,"List 7", R.array.list_7)
-            MainActivity.markList(context,"List 8", R.array.list_8)
-            MainActivity.markList(context,"List 9", R.array.list_9)
-            MainActivity.markList(context,"List 10", R.array.list_10)
+            MainActivity.markList(context, "List 1", R.array.list_1, "list1Done")
+            MainActivity.markList(context,"List 2", R.array.list_2, "list2Done")
+            MainActivity.markList(context,"List 3", R.array.list_3, "list3Done")
+            MainActivity.markList(context,"List 4", R.array.list_4, "list4Done")
+            MainActivity.markList(context,"List 5", R.array.list_5, "list5Done")
+            MainActivity.markList(context,"List 6", R.array.list_6, "list6Done")
+            MainActivity.markList(context,"List 7", R.array.list_7, "list7Done")
+            MainActivity.markList(context,"List 8", R.array.list_8, "list8Done")
+            MainActivity.markList(context,"List 9", R.array.list_9, "list9Done")
+            MainActivity.markList(context,"List 10", R.array.list_10, "list10Done")
             Toast.makeText(context,"Lists Marked!", Toast.LENGTH_LONG)
                     .show()
             MainActivity.prefEditString(context, "dateClicked", today)
