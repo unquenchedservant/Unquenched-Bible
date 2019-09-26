60896b8a8249935f1b8dcffd9ecf22af457e1561
Chillhumanoid <jonathanthorne13@gmail.com>
2019-09-26 15:25:26 -0400

    better code practice for resetList

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/dailyCheck.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/dailyCheck.kt
@@ -1,21 +1,14 @@
-package com.theunquenchedservant.pghsystem.ui.notifications
+package com.theunquenchedservant.granthornersbiblereadingsystem.ui.notifications
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-<<<<<<<< HEAD:app/src/main/java/com/theunquenchedservant/pghsystem/ui/notifications/dailyCheck.kt
-import com.theunquenchedservant.pghsystem.sharedPref.listNumberEditInt
-import com.theunquenchedservant.pghsystem.sharedPref.listNumberReadInt
-import com.theunquenchedservant.pghsystem.sharedPref.statisticsEdit
-import com.theunquenchedservant.pghsystem.sharedPref.statisticsRead
-========
 import androidx.preference.PreferenceManager
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity.Companion.log
 import com.theunquenchedservant.granthornersbiblereadingsystem.sharedPref.listNumberEditInt
 import com.theunquenchedservant.granthornersbiblereadingsystem.sharedPref.listNumberReadInt
 import com.theunquenchedservant.granthornersbiblereadingsystem.sharedPref.statisticsEdit
 import com.theunquenchedservant.granthornersbiblereadingsystem.sharedPref.statisticsRead
->>>>>>>> changed package name, begun implementing firebase:app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/dailyCheck.kt
 
 class dailyCheck : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -40,65 +33,42 @@ class dailyCheck : BroadcastReceiver() {
         }
         //RESETS ALL DONE LISTS AT MIDNIGHT EACH NIGHT
         when(listNumberReadInt(context, "list1Done")){
-            1->{
-                listNumberEditInt(context, "List 1", listNumberReadInt(context, "List 1") + 1);
-                listNumberEditInt(context, "list1Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 1", "list1Done")
+            }}
         when(listNumberReadInt(context, "list2Done")){
-            1->{
-                listNumberEditInt(context, "List 2", listNumberReadInt(context, "List 2") + 1);
-                listNumberEditInt(context, "list2Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 2", "list2Done")
+            }}
         when(listNumberReadInt(context, "list3Done")){
-            1->{
-                listNumberEditInt(context, "List 3", listNumberReadInt(context, "List 3") + 1);
-                listNumberEditInt(context, "list3Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 3", "list3Done")
+            }}
         when(listNumberReadInt(context, "list4Done")){
-            1->{
-                listNumberEditInt(context, "List 4", listNumberReadInt(context, "List 4") + 1)
-                listNumberEditInt(context, "list4Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 4", "list4Done")
+            }}
         when(listNumberReadInt(context, "list5Done")){
-            1->{
-                listNumberEditInt(context, "List 5", listNumberReadInt(context, "List 5") + 1)
-                listNumberEditInt(context, "list5Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 5", "list5Done")
+            }}
         when(listNumberReadInt(context, "list6Done")){
-            1->{
-                listNumberEditInt(context, "List 6", listNumberReadInt(context, "List 6") + 1)
-                listNumberEditInt(context, "list6Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 6", "list6Done")
+            }}
         when(listNumberReadInt(context, "list7Done")){
-            1->{
-                listNumberEditInt(context, "List 7", listNumberReadInt(context, "List 7") + 1)
-                listNumberEditInt(context, "list7Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 7", "list7Done")
+            }}
         when(listNumberReadInt(context, "list8Done")){
-            1->{
-                listNumberEditInt(context, "List 8", listNumberReadInt(context, "List 8") + 1)
-                listNumberEditInt(context, "list8Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 8", "list8Done")
+            }}
         when(listNumberReadInt(context, "list9Done")){
-            1->{
-                listNumberEditInt(context, "List 9", listNumberReadInt(context, "List 9") + 1)
-                listNumberEditInt(context, "list9Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 9", "list9Done")
+            }}
         when(listNumberReadInt(context, "list10Done")){
-            1->{
-                listNumberEditInt(context, "List 10", listNumberReadInt(context, "List 10") + 1)
-                listNumberEditInt(context, "list10Done", 0)
-            }
-        }
+            1->{ resetList(context, "List 10", "list10Done")
+            }}
         listNumberEditInt(context, "listsDone", 0)
+    }
+    private fun resetList(context: Context, listName: String, listNameDone: String){
+        log("Resetting $listName for dailyCheck")
+        listNumberEditInt(context, listName, listNumberReadInt(context, listName) + 1)
+        log("$listName index is now ${listNumberReadInt(context, listName)}")
+        listNumberEditInt(context, listNameDone, 0)
+        log("$listNameDone set to 0")
     }
 }
