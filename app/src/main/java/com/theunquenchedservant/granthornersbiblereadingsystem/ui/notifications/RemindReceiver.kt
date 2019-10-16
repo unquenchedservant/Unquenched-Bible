1954f737c522a140813cbf67f5411736ce55ea33
Chillhumanoid <jonathanthorne13@gmail.com>
2019-10-16 07:02:42 -0400

    Mass commit for changes

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/RemindReceiver.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/notifications/RemindReceiver.kt
@@ -6,15 +6,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
-import androidx.preference.Preference
 import androidx.preference.PreferenceManager
 
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity.Companion.log
 import com.theunquenchedservant.granthornersbiblereadingsystem.R
-import com.theunquenchedservant.granthornersbiblereadingsystem.sharedPref.listNumberReadInt
+import com.theunquenchedservant.granthornersbiblereadingsystem.SharedPref.listNumberReadInt
 
-class remindReceiver : BroadcastReceiver() {
+class RemindReceiver : BroadcastReceiver() {
     private var mNotificationManager: NotificationManager? = null
     override fun onReceive(context: Context, intent: Intent) {
         val vacation = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("vacation_mode", false)
@@ -22,26 +21,26 @@ class remindReceiver : BroadcastReceiver() {
         when(vacation) {
             false -> {
                 log("Vacation mode off, preparing reminder notification")
-                mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                val check = listNumberReadInt(context, "listsDone")
-                log("lists done so far = $check")
-                val allow_partial = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("allow_partial_switch", false)
-                log("Allow partial is $allow_partial")
-                when(check){
-                    0 -> {
-                        log("Sending notification to do reading (not partial)")
-                        deliverNotification(context, false)
-
-                    }
-                    in 1..9 -> {
-                        if(allow_partial) {
-                            log("$check lists done, allow partial is on")
-                            deliverNotification(context, true)
-                        }else if(!allow_partial){
-                            log("$check lists done, allow partial is off")
+                if(PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notif_switch", false)) {
+                    log("Vacation mode off, sending notification")
+                    val check = listNumberReadInt(context, "listsDone")
+                    log("lists done so far = $check")
+                    val allowPartial = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("allow_partial_switch", false)
+                    log("Allow partial is $allowPartial")
+                    mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    when (check) {
+                        0 -> {
+                            deliverNotification(context, false)
+                        }
+                        in 0..9 -> {
                             deliverNotification(context, true)
                         }
+                        10 ->{
+                            log("All lists done, not sending notification")
+                        }
                     }
+                }else{
+                    log("Notification switch off, not sending notification")
                 }
             }
             true -> {
@@ -53,14 +52,18 @@ class remindReceiver : BroadcastReceiver() {
     private fun deliverNotification(context: Context, partial:Boolean) {
         val rem = context.resources.getString(R.string.remTitle)
         var content : String? = null
-        if(partial){content="Don't forget to finish the reading!"}else{content="Don't forget to do the reading"}
+        content = if(partial){
+            "Don't forget to finish the reading!"
+        }else{
+            "Don't forget to do the reading"
+        }
         val tapIntent = Intent(context, MainActivity::class.java)
         tapIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         val tapPending = PendingIntent.getActivity(context, 0, tapIntent, 0)
-        val detailsIntent = Intent(context, doneReceiver::class.java)
+        val detailsIntent = Intent(context, DoneReceiver::class.java)
         val detailsPendingIntent = PendingIntent.getBroadcast(context, 0, detailsIntent, 0)
         val builder = NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setSmallIcon(R.drawable.ic_notification_icon)
                 .setContentTitle(rem)
                 .setContentText(content)
                 .addAction(android.R.drawable.ic_menu_save, "Done", detailsPendingIntent)
@@ -70,7 +73,7 @@ class remindReceiver : BroadcastReceiver() {
     }
 
     companion object {
-        private val NOTIFICATION_ID = 1
-        private val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+        private const val NOTIFICATION_ID = 1
+        private const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
     }
 }
