7b9a554953e4810d31fc57b266fadb7cae6a369e
Chillhumanoid <jonathanthorne13@gmail.com>
2019-10-16 07:47:49 -0400

    Added better viewmodel for home, with livedata

---
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/home/HomeView.kt
@@ -0,0 +1,37 @@
+package com.theunquenchedservant.granthornersbiblereadingsystem.ui.home
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeView : ViewModel() {
+    val list1: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list2: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list3: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list4: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list5: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list6: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list7: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list8: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list9: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+    val list10: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+}
