ccf26c4ea28e8030d100b462aabe124fd2b23300
Chillhumanoid <jonathanthorne13@gmail.com>
2019-11-15 13:28:23 -0500

    Better secured API Keys

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ESV/ESVGet.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ESV/ESVGet.kt
@@ -1,6 +1,7 @@
 package com.theunquenchedservant.granthornersbiblereadingsystem.ESV
 
 import android.content.Context
+import android.util.Base64
 import android.webkit.WebView
 import com.android.volley.RequestQueue
 import com.android.volley.Response
@@ -8,14 +9,24 @@ import com.android.volley.toolbox.BasicNetwork
 import com.android.volley.toolbox.DiskBasedCache
 import com.android.volley.toolbox.HurlStack
 import com.android.volley.toolbox.JsonObjectRequest
+import com.theunquenchedservant.granthornersbiblereadingsystem.App
 import com.theunquenchedservant.granthornersbiblereadingsystem.BuildConfig
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity
 
 object ESVGet {
+    init{
+        System.loadLibrary("native-lib")
+    }
+    external fun getESVKey() : String
+
+
+
     fun getESV(ctx: Context?, url: String): WebView {
+        val key = String(Base64.decode(getESVKey(), Base64.DEFAULT))
         var html : String?
-        val wv = WebView(ctx!!)
-        val cache = DiskBasedCache(ctx.cacheDir, 1024 * 1024)
+        val context = App.applicationContext()
+        val wv = WebView(ctx)
+        val cache = DiskBasedCache(context.cacheDir, 1024 * 1024)
         val network = BasicNetwork(HurlStack())
         val requestQueue = RequestQueue(cache, network).apply {
             start()
@@ -32,7 +43,7 @@ object ESVGet {
                 }) {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
-                headers["Authorization"] = BuildConfig.ESV
+                headers["Authorization"] = key
                 return headers
             }
         }
