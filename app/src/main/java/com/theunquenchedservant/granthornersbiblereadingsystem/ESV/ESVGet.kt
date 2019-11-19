c16e1799f2f9b321c42b0fab2e6a544b8c4108e2
Chillhumanoid <jonathanthorne13@gmail.com>
2019-11-19 14:34:08 -0500

    cleaned up ESV code

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ESV/ESVGet.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ESV/ESVGet.kt
@@ -11,8 +11,6 @@ import com.android.volley.toolbox.DiskBasedCache
 import com.android.volley.toolbox.HurlStack
 import com.android.volley.toolbox.JsonObjectRequest
 import com.theunquenchedservant.granthornersbiblereadingsystem.App
-import com.theunquenchedservant.granthornersbiblereadingsystem.BuildConfig
-import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity
 import com.theunquenchedservant.granthornersbiblereadingsystem.MainActivity.Companion.log
 import java.util.*
 import kotlin.collections.HashMap
@@ -39,40 +37,6 @@ object ESVGet {
             url = "https://api.esv.org/v3/passage/html/?q=$chapter&include-css-link=true&inline-styles=false&wrapping-div=false&div-classes=passage&include-passage-references=false&include-footnotes=false&include-copyright=true&include-short-copyright=false"
         }
         val wv = getESV(ctx, url)
-        chapterShower(wv, title, psalms, iteration, ctx)
-    }
-
-    fun getESV(ctx: Context?, url: String): WebView {
-        val key = String(Base64.decode(getESVKey(), Base64.DEFAULT))
-        var html : String?
-        val context = App.applicationContext()
-        val wv = WebView(ctx)
-        val cache = DiskBasedCache(context.cacheDir, 1024 * 1024)
-        val network = BasicNetwork(HurlStack())
-        val requestQueue = RequestQueue(cache, network).apply {
-            start()
-        }
-        val jsonObjectRequest = object: JsonObjectRequest(Method.GET, url, null,
-                Response.Listener { response ->
-                    html = response.getJSONArray("passages").getString(0)
-                    html = html!!.replace("\"http://static.esvmedia.org.s3.amazonaws.com/tmp/text.css\"", "esv.css")
-                    wv.loadDataWithBaseURL("file:///android_asset/", html,"text/html", "UTF-8", null)
-                },
-
-                Response.ErrorListener { error ->
-                    MainActivity.log("ERROR: $error")
-                }) {
-            override fun getHeaders(): MutableMap<String, String> {
-                val headers = HashMap<String, String>()
-                headers["Authorization"] = key
-                return headers
-            }
-        }
-        requestQueue.add(jsonObjectRequest)
-        return wv
-    }
-
-    private fun chapterShower(wv : WebView, title: String, psalms: Boolean, iteration: Int, ctx: Context?){
         val alert = AlertDialog.Builder(ctx!!)
         alert.setTitle(title)
         alert.setView(wv)
@@ -95,6 +59,36 @@ object ESVGet {
             }
         }
         alert.show()
+    }
+
+    fun getESV(ctx: Context?, url: String): WebView {
+        val key = String(Base64.decode(getESVKey(), Base64.DEFAULT))
+        var html : String?
+        val context = App.applicationContext()
+        val wv = WebView(ctx)
+        val cache = DiskBasedCache(context.cacheDir, 1024 * 1024)
+        val network = BasicNetwork(HurlStack())
+        val requestQueue = RequestQueue(cache, network).apply {
+            start()
+        }
+        val jsonObjectRequest = object: JsonObjectRequest(Method.GET, url, null,
+                Response.Listener { response ->
+                    html = response.getJSONArray("passages").getString(0)
+                    html = html!!.replace("\"http://static.esvmedia.org.s3.amazonaws.com/tmp/text.css\"", "esv.css")
+                    wv.loadDataWithBaseURL("file:///android_asset/", html,"text/html", "UTF-8", null)
+                },
+
+                Response.ErrorListener { error ->
+                    log("ERROR: $error")
+                }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = key
+                return headers
+            }
+        }
+        requestQueue.add(jsonObjectRequest)
+        return wv
     }
 
 }
