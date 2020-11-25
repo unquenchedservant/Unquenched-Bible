deaf6772b763df1a50ab033964392fed9fa9de6d
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-11-25 15:04:59 -0500

    misc. gradle changes for updating packages

---
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/data/FirestoreRepository.kt
@@ -0,0 +1,4 @@
+package com.theunquenchedservant.granthornersbiblereadingsystem.data
+
+class FirestoreRepository {
+}
---
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/data/ReadingItem.kt
@@ -0,0 +1,3 @@
+package com.theunquenchedservant.granthornersbiblereadingsystem.data
+import androidx.databinding.Observable
+data class ObservableListProfile(val name: String, val reading: Observable)
