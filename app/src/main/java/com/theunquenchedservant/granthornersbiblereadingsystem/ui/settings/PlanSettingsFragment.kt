1b1d1f49bc08d940482c405ec9b099b4133eacce
Jonathan Thorne <jonathanthorne13@gmail.com>
2020-11-27 19:38:22 -0500

    added a screen for plan settings

---
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/settings/PlanSettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.theunquenchedservant.granthornersbiblereadingsystem.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class PlanSettingsFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        
+    }
+}
