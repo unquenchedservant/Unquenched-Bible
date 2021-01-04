2b796145bd5040012994cef2470de5236ec9f680
Jonathan Thorne <contact@theunquenchedservant.com>
2021-01-04 10:25:55 -0500

    added survey to settings

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/settings/MainSettings.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/settings/MainSettings.kt
@@ -37,6 +37,7 @@ class MainSettings : PreferenceFragmentCompat() {
         val account: Preference? = findPreference("googleSignIn")
         val darkMode: Preference? = findPreference("darkMode")
         val discord: Preference? = findPreference("discordLink")
+        val survey: Preference? = findPreference("surveyLink")
         val mainActivity = activity as MainActivity
 
         val dark = getBoolPref("darkMode")
@@ -119,6 +120,11 @@ class MainSettings : PreferenceFragmentCompat() {
         }
         discord!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
             val i = Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://discord.gg/AKrefXRyuA"))
+            startActivity(i)
+            false
+        }
+        survey!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            val i = Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://forms.gle/euZ2QDjR5hwnVQSy6"))
             startActivity(i)
             false
         }
