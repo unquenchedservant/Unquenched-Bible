a1cfff355cc2b8c25028b9562ccb4de4a1e9f9ca
Jonathan Thorne <jonathanthorne13@gmail.com>
2021-01-21 16:10:18 -0500

    added ids for mcheyne lists

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/data/ReadingListRepository.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/data/ReadingListRepository.kt
@@ -59,6 +59,10 @@ class ReadingListRepository {
             "list8"-> R.array.list_8
             "list9"-> R.array.list_9
             "list10"-> R.array.list_10
+            "mcheyne_list1"->R.array.mcheyne_list1
+            "mcheyne_list2"->R.array.mcheyne_list2
+            "mcheyne_list3"->R.array.mcheyne_list3
+            "mcheyne_list4"->R.array.mcheyne_list4
             else-> 0
         }
     }
