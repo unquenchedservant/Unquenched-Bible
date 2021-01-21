b42a108c92b86048468a06e2da59500688f75bf3
Jonathan Thorne <jonathanthorne13@gmail.com>
2021-01-21 11:49:06 -0500

    added plan system to plan settings

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/settings/PlanSettingsFragment.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/settings/PlanSettingsFragment.kt
@@ -19,6 +19,7 @@ class PlanSettingsFragment: PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.plan_preferences, rootKey)
         val mainActivity = activity as MainActivity
+        val planMethod: Preference? = findPreference("planMethod")
         val psalms: Preference? = findPreference("psalms")
         val holdPlan: Preference? = findPreference("holdTilAll")
         val partialStreakAllow : Preference? = findPreference("allow_partial_switch")
@@ -28,8 +29,10 @@ class PlanSettingsFragment: PreferenceFragmentCompat() {
         translation.setEntryValues(R.array.translationArray)
         var currentTranslation = getStringPref("bibleVersion", "ESV")
         translation.value = currentTranslation
-        planType!!.summary = "${getString(R.string.summary_reading_type)} Current Method: ${getStringPref("planType", "horner").capitalize()}"
+        planType!!.summary = "${getString(R.string.summary_plan_type)} Current Plan: ${getStringPref("planSystem", "pgh").toUpperCase()}"
         planType.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_navigate_next_24, mainActivity.theme)
+        planMethod!!.summary = "${getString(R.string.summary_reading_type)} Current Method: ${getStringPref("planType", "horner").capitalize()}"
+        planMethod.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_navigate_next_24, mainActivity.theme)
         translation.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_arrow_drop_down_24, mainActivity.theme)
         when (getStringPref("planType", "horner")){
             "horner"->{
@@ -57,7 +60,7 @@ class PlanSettingsFragment: PreferenceFragmentCompat() {
         if(partial){
             partialStreakAllow!!.setDefaultValue(true)
         }
-        planType.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+        planMethod.onPreferenceClickListener = Preference.OnPreferenceClickListener {
             mainActivity.navController.navigate(R.id.navigation_plan_type)
             true
         }
