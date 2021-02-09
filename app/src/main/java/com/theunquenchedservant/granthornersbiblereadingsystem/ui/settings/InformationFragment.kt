3ca18cf5cf9fbe41571d2e94542f90b873e07572
Jonathan Thorne <jonathanthorne13@gmail.com>
2021-02-09 16:24:04 -0500

    Added information in support email template for better debugging

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/settings/InformationFragment.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/settings/InformationFragment.kt
@@ -8,8 +8,13 @@ import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.theunquenchedservant.granthornersbiblereadingsystem.App
 import com.theunquenchedservant.granthornersbiblereadingsystem.R
+import com.theunquenchedservant.granthornersbiblereadingsystem.utilities.SharedPref.getBoolPref
+import com.theunquenchedservant.granthornersbiblereadingsystem.utilities.SharedPref.getIntPref
+import com.theunquenchedservant.granthornersbiblereadingsystem.utilities.SharedPref.getStringPref
 
 class InformationFragment : PreferenceFragmentCompat() {
 
@@ -65,11 +70,25 @@ class InformationFragment : PreferenceFragmentCompat() {
             false
         }
         contact!!.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+            var emailText = "\n\n(Please leave the following)\n\n"
+            emailText += "Version: ${getIntPref("versionNumber")}\n"
+            emailText += "Plan System: ${getStringPref("planSystem")}\n"
+            emailText += "Plan Method: ${getStringPref("planType")}\n"
+            val psText = if(getBoolPref("psalms")) "5Psalms" else getIntPref("list6").toString()
+            emailText += "PGH Lists: (${getIntPref("list1")}, ${getIntPref("list2")}, ${getIntPref("list3")}, ${getIntPref("list4")}, ${getIntPref("list5")}, $psText, ${getIntPref("list7")}, ${getIntPref("list8")}, ${getIntPref("list9")}, ${getIntPref("list10")})\n"
+            emailText += "MCheyne Lists: (${getIntPref("mcheyneList1")}, ${getIntPref("mcheyneList2")}, ${getIntPref("mcheyneList3")}, ${getIntPref("mcheyneList4")})\n"
+            emailText += "Indexes: (${getIntPref("currentDayIndex")}, ${getIntPref("mcheyneCurrentDayIndex")})\n"
+            emailText += "PGH Lists Done: ${getIntPref("listsDone")}\n"
+            emailText += "Mcheyne Lists Done: ${getIntPref("mcheyneListsDone")}\n"
+            emailText += "Phone: ${android.os.Build.PRODUCT}\n"
+            emailText += "Android Version: ${android.os.Build.VERSION.RELEASE}\n"
+            emailText += "ID: ${Firebase.auth.currentUser?.uid}"
             val i = Intent(Intent.ACTION_SENDTO)
                     .setType("text/plain")
                     .setData(Uri.parse("mailto:"))
                     .putExtra(Intent.EXTRA_EMAIL, arrayOf("contact@unquenched.tech"))
-                    .putExtra(Intent.EXTRA_SUBJECT, "COMMENT/QUESTION - PGH APP")
+                    .putExtra(Intent.EXTRA_TEXT, emailText)
+                    .putExtra(Intent.EXTRA_SUBJECT, "COMMENT/QUESTION - PGH APP (VERSION ${getIntPref("versionNumber")})")
             try {
                 startActivity(i)
             }catch(e:ActivityNotFoundException){
