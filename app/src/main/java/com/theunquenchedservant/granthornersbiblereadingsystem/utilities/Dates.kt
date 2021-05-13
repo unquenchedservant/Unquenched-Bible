adc0b3b386f03eda5d8257d1c2b1eaab4906439c
Jonathan Thorne <jonathanthorne13@gmail.com>
2021-05-13 09:47:31 -0400

    re-added old date functions so that I can min SDK 24

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/utilities/Dates.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/utilities/Dates.kt
@@ -1,24 +1,41 @@
 package com.theunquenchedservant.granthornersbiblereadingsystem.utilities
 
+import android.annotation.SuppressLint
+import android.os.Build
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.Month
 import java.time.format.DateTimeFormatter
 import com.theunquenchedservant.granthornersbiblereadingsystem.utilities.Log.debugLog
+import java.text.SimpleDateFormat
+import java.util.*
 
 object Dates {
 
     fun isLeapDay():Boolean{
-        val today = LocalDate.now()
-        return today.month == Month.FEBRUARY && today.dayOfMonth == 29
-
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val today = LocalDate.now()
+            today.month == Month.FEBRUARY && today.dayOfMonth == 29
+        }else{
+            val today = Calendar.getInstance()
+            today.get(Calendar.MONTH) == Calendar.FEBRUARY && today.get(Calendar.DAY_OF_MONTH) == 29
+        }
     }
     fun isWeekend():Boolean{
-        val today = LocalDate.now()
-        return when(today.dayOfWeek){
-            DayOfWeek.SATURDAY, DayOfWeek.SUNDAY -> true
-            else -> false
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val today = LocalDate.now()
+            when (today.dayOfWeek) {
+                DayOfWeek.SATURDAY, DayOfWeek.SUNDAY -> true
+                else -> false
+            }
+        }else{
+            val today = Calendar.getInstance()
+            when (today.get(Calendar.DAY_OF_WEEK)) {
+                Calendar.SATURDAY, Calendar.SUNDAY -> true
+                else -> false
+            }
         }
+
     }
 
     fun checkDate(date:String, option: String, fullMonth: Boolean): Boolean{
@@ -29,20 +46,40 @@ object Dates {
             else -> false
         }
     }
+    @SuppressLint("SimpleDateFormatDetector")
     fun getDate(option: Int, fullMonth: Boolean): String {
-        val date : LocalDate
-        val now  = LocalDate.now()
-        date = when(option){
-            0 -> now
-            1 -> now.minusDays(1)
-            else -> now
-        }
-        return if (fullMonth) {
-            debugLog("THIS IS THE FORMATTED DATE ${date.format(DateTimeFormatter.ofPattern("MMMM dd"))}")
-            date.format(DateTimeFormatter.ofPattern("MMMM dd"))
-        } else {
-            debugLog("THIS IS THE FORMATTED DATE ${date.format(DateTimeFormatter.ofPattern("MMMM dd"))}")
-            date.format(DateTimeFormatter.ofPattern("MMM dd"))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val date: LocalDate
+            val now = LocalDate.now()
+            date = when (option) {
+                0 -> now
+                1 -> now.minusDays(1)
+                else -> now
+            }
+            return if (fullMonth) {
+                debugLog("THIS IS THE FORMATTED DATE ${date.format(DateTimeFormatter.ofPattern("MMMM dd"))}")
+                date.format(DateTimeFormatter.ofPattern("MMMM dd"))
+            } else {
+                debugLog("THIS IS THE FORMATTED DATE ${date.format(DateTimeFormatter.ofPattern("MMMM dd"))}")
+                date.format(DateTimeFormatter.ofPattern("MMM dd"))
+            }
+        }else{
+            val date: Date
+            val now: Date = Calendar.getInstance().time
+            val yesterday = Calendar.getInstance()
+            yesterday.set(Calendar.HOUR_OF_DAY, 0)
+            yesterday.add(Calendar.DATE, -1)
+
+            date = when(option){
+                0 -> now
+                1 -> { yesterday.time }
+                else -> now
+            }
+            return if(fullMonth){
+                SimpleDateFormat("MMMM dd", Locale.US).format(date)
+            }else{
+                SimpleDateFormat("MMM dd", Locale.US).format(date)
+            }
         }
     }
 }
