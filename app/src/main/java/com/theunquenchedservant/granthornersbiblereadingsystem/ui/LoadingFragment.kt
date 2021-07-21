06cfb9b3d9ea711435a3f1cd75028cfcf8dc3a34
Jonathan Thorne <jonathanthorne13@gmail.com>
2021-07-20 22:17:33 -0400

    Added a loading screen

--- app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/LoadingFragment.kt
+++ app/src/main/java/com/theunquenchedservant/granthornersbiblereadingsystem/ui/LoadingFragment.kt
@@ -1,4 +1,18 @@
 package com.theunquenchedservant.granthornersbiblereadingsystem.ui
 
-class LoadingFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.theunquenchedservant.granthornersbiblereadingsystem.R
+
+class LoadingFragment: Fragment()  {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_loading, container, false)
+    }
 }
